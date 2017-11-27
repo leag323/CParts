6b86262bf70d0691c247add4a7cc123dfcfa7cb7
leag323 <leag3@bezeqint.net>
2017-11-27 22:23:13 +0200

    add services: app/model/user.js, activeUser,js

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,5 +1,5 @@
 
-app.controller("loginCtrl", function($scope, $location) {
+app.controller("loginCtrl", function($scope, $http, $location, activeUser, User) {
     $scope.test = "Hello Home";  
     $scope.login = function() {
         console.log("login function");
--- app/model/user.js
+++ app/model/user.js
@@ -1,5 +1,5 @@
 // Shared User Constructor
-recipeApp.factory("User", function(){
+app.factory("User", function(){
     function User(plainObject) {
         this.email = plainObject.email;
         this.password = plainObject.password;
@@ -9,32 +9,4 @@ recipeApp.factory("User", function(){
     };
 
     return User;
-});
-
-// Service that manges the active user
-recipeApp.factory("activeUser", function(User){
-    var user = null;
-
-    var isLoggedIn = function() {
-        return user ? true : false;
-    };
-
-    var login = function(loggedInUser) {
-        user = loggedInUser;
-    };
-
-    var logout = function() {
-        user = null;
-    };
-
-    var get = function() {
-        return user;
-    };
-
-    return {
-        isLoggedIn: isLoggedIn,
-        login: login,
-        logout: logout,
-        get: get
-    };   
 });
