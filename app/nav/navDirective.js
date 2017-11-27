1bd0121d316bfa9cfb0755c1e48ffc5c196c8af6
leag323 <leag3@bezeqint.net>
2017-11-27 17:28:33 +0200

    add nav bar, po.html+controller

---
+++ app/nav/navDirective.js
@@ -0,0 +1,7 @@
+
+app.directive("cpartsNavbar", function() {
+    return {
+        templateUrl: "app/nav/nav.html",
+        controller: "navCtrl"
+    }
+})
---
+++ app/po/poCtrl.js
@@ -0,0 +1,11 @@
+app.controller("poCtrl", function($scope, $http, $log, $location) {
+  /*
+  $http.get("app/model/data/actors.json").then(function mySuccess(response) {
+            console.log("success open file actors.json");
+            actors.setActors(response.data);                           
+            $scope.actors = actors.getAll();                                                          
+        },  function myError(response) {
+            console.log("error open file actors.json");
+        });
+        */
+});
