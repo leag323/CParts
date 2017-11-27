800af9504672677d835d7da44c915be08c2ff3e4
leag323 <leag3@bezeqint.net>
2017-11-27 22:13:51 +0200

    update loginCtrl add $scope.login

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,14 +1,5 @@
 
 app.controller("homeCtrl", function($scope, $location) {
     $scope.test = "Hello Home";  
-    /*
-    $location.path("/");
-    */
-    /*
-    $scope.login = function() {
-      $uibModal.open({
-          templateUrl: "app/login/login.html",
-          controller: "LoginCtrl"
-      })
-  } */
+    
   });
--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,5 +1,7 @@
 
 app.controller("loginCtrl", function($scope, $location) {
     $scope.test = "Hello Home";  
-    
+    $scope.login = function() {
+        console.log("login function");
+    };
   });
