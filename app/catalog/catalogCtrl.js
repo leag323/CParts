2314c0cb614dcf39de124154c07caa978e869057
leag323 <leag3@bezeqint.net>
2017-11-28 12:46:02 +0200

    show item desc

--- app/catalog/catalogCtrl.js
+++ app/catalog/catalogCtrl.js
@@ -2,9 +2,9 @@ app.controller("catalogCtrl", function($scope, $http, $log, $location, items) {
  
     $http.get("app/data/buyer-catalog.json").then(function mySuccess(response) {
             console.log("success open file buyer-catalog.json");    
-            $scope.itemsArr = [];
+            $scope.items = [];
             items.load(response.data);                           
-            $scope.itemsArr = items.getAll();                                                   
+            $scope.items = items.getAll();                                                   
         },  function myError(response) {
             console.log("error open file actors.json");
         });
