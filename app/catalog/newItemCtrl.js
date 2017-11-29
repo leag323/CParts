5ecc97fedbc7dda2859d108a53e3f390ba6665dc
leag323 <leag3@bezeqint.net>
2017-11-29 11:52:48 +0200

    update newItemCtrl.js

--- app/catalog/newItemCtrl.js
+++ app/catalog/newItemCtrl.js
@@ -1,8 +1,20 @@
-app.controller("newItemCtrl", function($scope, $http, $log, $location, activeUser) {
+app.controller("newItemCtrl", function($scope, $http, $log, $location, activeUser, Item) {
     // If the user is not logged in going back to home screen  
       if (!activeUser.isLoggedIn()) {
           $location.path("/");
           return;
       }  
+
+    $scope.item = new Item({});
+
+    $scope.cancel = function () {
+        $location.path("/catalog");
+    }
+
+    $scope.create = function () {
+        items.add($scope.item);
+        $location.path("/catalog");
+    }
+
   });
   
