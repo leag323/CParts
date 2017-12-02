65e163040b0536bc89dce318dbe773e0c085ec28
leag323 <leag3@bezeqint.net>
2017-12-02 18:54:34 +0200

    validation when create new item newItemCtrl.js

--- app/catalog/newItemCtrl.js
+++ app/catalog/newItemCtrl.js
@@ -25,8 +25,24 @@ app.controller("newItemCtrl", function($scope, $http, $log, $location, activeUse
     }
 
     $scope.create = function () {
-        if ($scope.item.itemMinQty > $scope.item.itemMaxQty){
-            window.alert("Maximum Qty must be great than Minimum Qty ");
+        if ($scope.item.itemNo === undefined){
+            $scope.validationError = true;
+            $scope.errorMessage = "Item Number is required";
+        } else if ($scope.item.itemDesc === undefined) {
+                   $scope.validationError = true;
+                   $scope.errorMessage = "Item Description is required";
+        } else if ($scope.item.itemUom === undefined) {
+                   $scope.validationError = true;
+                   $scope.errorMessage = "Item UOM is required";
+        } else if ($scope.item.itemMinQty === undefined) {
+                   $scope.validationError = true;
+                   $scope.errorMessage = "Item Minimum Quantity is required";
+        } else if ($scope.item.itemMaxQty === undefined) {
+                    $scope.validationError = true;
+                    $scope.errorMessage = "Item Maximum Quantity is required";
+        } else if ($scope.item.itemMinQty > $scope.item.itemMaxQty) {
+                   $scope.validationError = true;
+                   $scope.errorMessage = "Maximum Qty must be great than Minimum Qty";        
         } else {
                 items.add($scope.item);
                 $location.path("/main");
