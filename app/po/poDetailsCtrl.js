e5295a247d04fcd58c2134a133f4afd039d351a8
leag323 <leag3@bezeqint.net>
2017-12-05 21:47:14 +0200

    update item desc+ item uom in podetails

--- app/po/poDetailsCtrl.js
+++ app/po/poDetailsCtrl.js
@@ -2,7 +2,7 @@
 	 we have 3 roles: buyer, storeKeeper, wip 
 	 only buyer can navigate to po details and update/delete	
 */
-app.controller("poDetailsCtrl", function($scope, $log, $location, $routeParams, activeUser, Order, orders) {
+app.controller("poDetailsCtrl", function($scope, $log, $location, $routeParams, activeUser, items, Order, orders) {
      // If the user is not logged in going back to home screen
     if (!activeUser.isLoggedIn()) {
         $location.path("/");
@@ -11,16 +11,25 @@ app.controller("poDetailsCtrl", function($scope, $log, $location, $routeParams, 
     
     $scope.errorMessage = "";
     $scope.message = "";
-    $scope.selectedItemDesc = "desc";
+    $scope.selectedItemDesc = "";    
+    $scope.selectedItemUom = "";
     $scope.validationError = false;    
     $scope.validationUpdate = false;    
     
     // Creating a copy of the order object so changes won't be reflected on the array
 
-    $scope.order = new Order(orders.getById($routeParams.index));
-    console.log("poDetailCtrl $routeParams.index: " + $routeParams.index);    
-    console.log("poDetailCtrl $scope.order.orderNo: " + $scope.order.orderNo);    
-
+    $scope.order = new Order(orders.getById($routeParams.index));        
+    $scope.selectedReqDate = new Date("1950-01-01");
+        
+    /*
+    $scope.selectedReqDate = function() {
+        return new Date(order.requiredDate)
+    };
+    */
+    //console.log("*** " +  $routeParams.index);
+    
+    $scope.selectedItemDesc = items.getByItemNo($scope.order.orderItemNo).itemDesc;
+    $scope.selectedItemUom = items.getByItemNo($scope.order.orderItemNo).itemUom;
     $scope.cancel = function() {
         console.log("poDetailCtrl cancel");
         $location.path("/po");        
